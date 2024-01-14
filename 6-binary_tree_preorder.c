768d8fb2d8ed56f53053c654aa5c015a8b5fe8a9
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-14 11:56:00 -0800

    tasks

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,7 +7,7 @@ binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (func && tree)
 	{
 		func(tree->n);
-		binary_tree_inorder(tree->left, func);
-		binary_tree_inorder(tree->right, func);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
