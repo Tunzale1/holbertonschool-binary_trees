21fef1b202750a2294a05f011fbb64f1d0b6e632
Tunzale1 <tunzaleimanova03@mail.ru>
2024-01-14 05:30:59 -0800

    must workk!

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,10 +8,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!tree->parent)
-		return (0);
-	else if (tree->left != NULL && tree->right != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
-		return (0);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
